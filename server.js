6e0ccbe4e765f589b2a286fc5f4cb20df55aaf58
jmateus <jmateus@jmateuss-iMac.local>
2020-07-08 11:19:18 +0100

    error handling

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const colors = require('colors');
+const errorHandler = require('./middleware/error')
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+ 
 const app = express();
+
+// Body Parser
+app.use(express.json());
 
 //Call Middleware
 //Dev logging Middleware, only run on developpement Mode
@@ -20,9 +28,21 @@ if (process.env.NODE_ENV === 'development') {
 // Mount  Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+
+// Error Handlers
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(
+const server = app.listen(
     PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV}on port ${PORT}!`);
+    console.log(`Server running in ${process.env.NODE_ENV}on port ${PORT}!`.yellow.bold);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    //Close Server & Exit process
+    server.close(() => process.exit(1));
+})
